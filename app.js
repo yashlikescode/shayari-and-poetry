e361190c12ecca08a8b8632d3e138a0d1e43cbf1
Yashasvi <kmryashasvi@gmail.com>
2023-08-20 20:39:47 +0530

    made file

--- app.js
+++ app.js
@@ -10,17 +10,41 @@
             card.style.width = `${cardHeight}px`;
     });
 
+// Replace 'your-csv-file.csv' with the actual path or URL of your CSV file
+const csvFilePath = './shayari.csv';
 
-    fetch("./shayari.csv")
-        .then(function(response) {
-            return response.text();
-        })
-        .then(function(csvData) {
-            // Process the CSV data here
-            console.log(csvData);
-        })
-        .catch(function(error) {
-            console.error('Error fetching CSV:', error);
-        });
+// Arrays to store the data from the first three columns
+const column1 = [];
+const column2 = [];
+const column3 = [];
+
+// Fetch the CSV file
+fetch(csvFilePath)
+  .then(response => response.text())
+  .then(csvData => {
+    // Split CSV data into lines
+    const lines = csvData.split('\n');
+
+    // Iterate through each line (row) of the CSV
+    lines.forEach(line => {
+      const columns = line.split(',');
+
+      // Extract values from the first three columns
+      if (columns.length >= 3) {
+        column1.push(columns[0]);
+        column2.push(columns[1]);
+        column3.push(columns[2]);
+      }
+    });
+
+    // Print or use the extracted arrays
+    console.log('Column 1:', column1);
+    console.log('Column 2:', column2);
+    console.log('Column 3:', column3);
+  })
+  .catch(error => {
+    console.error('Error fetching or processing the CSV:', error);
+  });
+
 
    
